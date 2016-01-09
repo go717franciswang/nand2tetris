6d564080225e638d791999932ef20a3b899b5b7c
Francis Wang <go717franciswang@gmail.com>
2016-01-09 15:14:34 -0800

    Project 4

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -3,9 +3,52 @@
 // by Nisan and Schocken, MIT Press.
 // File name: projects/04/Fill.asm
 
-// Runs an infinite loop that listens to the keyboard input. 
+// Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel. When no key is pressed, the
 // program clears the screen, i.e. writes "white" in every pixel.
 
-// Put your code here.
+(INIF_LOOP)
+
+  @SCREEN     // i = 16384
+  D=A
+  @i
+  M=D
+
+  @24576      // keyboard
+  D=M
+  @KEYDOWN
+  D;JNE       // if keyboard <> 0 goto KEYDOWN
+  @color
+  M=0
+  @END_KEYDOWN
+  0;JMP
+  (KEYDOWN)
+  @color
+  M=-1
+  (END_KEYDOWN)
+
+  @24576      // end = 16384+256*32
+  D=A
+  @end
+  M=D
+
+  (LOOP)
+    @color    // M[i] = color
+    D=M
+    @i
+    A=M
+    M=D
+
+    @i        // i = i+1
+    M=M+1
+
+    @i        // if i-end < 0 goto LOOP
+    D=M
+    @end
+    D=D-M
+    @LOOP
+    D;JLT
+
+@INIF_LOOP
+0;JMP
--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -6,4 +6,52 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+  @i        // i=0
+  M=0
+  @R2       // R2=0
+  M=0
+
+  @R1       // if R1 < 0 goto NEGATIVE
+  D=M
+  @NEGATIVE
+  D;JLT
+  @inc
+  M=1
+  @R0       // mult=R0
+  D=M
+  @mult
+  M=D
+  @END_NEGATIVE
+  0;JMP
+(NEGATIVE)
+  @inc
+  M=-1
+  @R0      // mult=-R0
+  D=-M
+  @mult
+  M=D
+(END_NEGATIVE)
+
+(LOOP)
+  @i        // if i-R1=0 goto END_LOOP
+  D=M
+  @R1
+  D=D-M
+  @END_LOOP
+  D;JEQ
+
+  @mult       // R2=R2+mult
+  D=M
+  @R2
+  M=M+D
+  @inc      // i=i+inc
+  D=M
+  @i
+  M=M+D
+  @LOOP
+  0;JMP
+(END_LOOP)
+
+(END)
+  @END
+  0;JMP
