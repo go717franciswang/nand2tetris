f26d33c8ed8e732aba8a8e0c70eb64232867b8b9
Francis Wang <go717franciswang@hotmail.com>
2016-01-25 23:37:55 -0800

    Working tokenizer

--- 10/tokenizer.py
+++ 10/tokenizer.py
@@ -1,17 +1,23 @@
 import re
 
 lexical_elements = [
-        ('comment', re.compile('//.*$|/\*.*\*/', re.MULTILINE)),
+        ('comment', re.compile('//.*$|/\*[\s\S]*?\*/', re.MULTILINE)),
         ('keyword', re.compile('class|constructor|function|method|field|static|var|int|char|boolean|void|true|false|null|this|let|do|if|else|while|return')),
         ('symbol', re.compile('[{}()[\].,;+\-*/&|<>=~]')),
         ('integerConstant', re.compile('\d{1,5}')),
         ('stringConstant', re.compile('"[^"\n]*"')),
         ('identifier', re.compile('[a-zA-Z_][a-zA-Z_0-9]*'))]
+xml_special_char = {
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        '&': '&amp;'
+        }
 
 def tokenize(filein):
+    tokens = []
     with open(filein) as f:
         content = f.read()
-        tokens = []
         while content != '':
             content = content.strip()
 
@@ -29,7 +35,11 @@ def tokenize(filein):
                         tokens.append((element, match))
                     break
 
-        print tokens
-
-
+        fileout = filein.split('.')[0] + '.T.xml'
+        fout = open(fileout, 'w')
+        fout.write('<tokens>\n')
+        for t, e in tokens:
+            fout.write('<%s> %s </%s>\n' % (t, xml_special_char.get(e, e), t))
+        fout.write('</tokens>\n')
+    return tokens
 
