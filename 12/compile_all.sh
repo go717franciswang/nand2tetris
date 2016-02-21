bc8dd23a67635a62c04acd41359f1f6f8e6a1975
Francis Wang <go717franciswang@hotmail.com>
2016-02-20 16:01:01 -0800

    Memory basic

--- 12/compile_all.sh
+++ 12/compile_all.sh
@@ -1,2 +1,3 @@
 bash ~/Downloads/nand2tetris/tools/JackCompiler.sh MathTest
 bash ~/Downloads/nand2tetris/tools/JackCompiler.sh StringTest
+bash ~/Downloads/nand2tetris/tools/JackCompiler.sh MemoryTest
