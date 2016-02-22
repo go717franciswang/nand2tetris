ada8ba9d46a64fd44de018eb68638863ccb28f17
Francis Wang <go717franciswang@hotmail.com>
2016-02-21 22:48:22 -0800

    Array

--- 12/compile_all.sh
+++ 12/compile_all.sh
@@ -1,3 +1,4 @@
 bash ~/Downloads/nand2tetris/tools/JackCompiler.sh MathTest
 bash ~/Downloads/nand2tetris/tools/JackCompiler.sh StringTest
 bash ~/Downloads/nand2tetris/tools/JackCompiler.sh MemoryTest
+bash ~/Downloads/nand2tetris/tools/JackCompiler.sh ArrayTest
