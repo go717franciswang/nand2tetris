a7649ca87f43f3ad97b95f7bd3fe0bd2371bbf10
Francis Wang <go717franciswang@hotmail.com>
2016-02-28 17:23:40 -0800

    Keyboard

--- 12/compile_all.sh
+++ 12/compile_all.sh
@@ -4,3 +4,5 @@ bash ~/Downloads/nand2tetris/tools/JackCompiler.sh MemoryTest
 bash ~/Downloads/nand2tetris/tools/JackCompiler.sh ArrayTest
 bash ~/Downloads/nand2tetris/tools/JackCompiler.sh OutputTest
 bash ~/Downloads/nand2tetris/tools/JackCompiler.sh ScreenTest
+bash ~/Downloads/nand2tetris/tools/JackCompiler.sh KeyboardTest
+bash ~/Downloads/nand2tetris/tools/JackCompiler.sh SysTest
